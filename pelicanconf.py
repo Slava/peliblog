7e8bf169fbd0c8ec1deceefc2add65272f518511
Slava Kim <slava@meteor.com>
2014-01-05 15:16:52 -0800

    New logo, hide comments

--- pelicanconf.py
+++ pelicanconf.py
@@ -17,6 +17,7 @@ PLUGINS = ['latex']
 GOOGLE_ANALYTICS = 'UA-37747647-1'
 DISQUS_SITENAME = 'zdeslavablog'
 TWITTER_USERNAME = 'imslavko'
+COLLAPSE_COMMENTS = True
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
