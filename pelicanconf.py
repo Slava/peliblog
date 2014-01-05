3a92fbed879b9d5286b79c44fb0cd23253081b2e
Slava Kim <slava@meteor.com>
2014-01-05 00:16:55 -0800

    Basic configuration
    - Test post
    - Latex

--- pelicanconf.py
+++ pelicanconf.py
@@ -3,6 +3,7 @@
 from __future__ import unicode_literals
 
 AUTHOR = u'Slava Kim'
+AUTHOR_BIO = u'Learning to code since 2008'
 SITENAME = u'Yet Another Dev Blog'
 SITEURL = ''
 
@@ -10,22 +11,32 @@ TIMEZONE = 'America/San_Francisco'
 
 DEFAULT_LANG = u'en'
 
+PLUGINS = ['latex']
+
+# More settings
+GOOGLE_ANALYTICS = 'UA-37747647-1'
+DISQUS_SITENAME = 'zdeslavablog'
+TWITTER_USERNAME = 'imslavko'
+
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 
 # Blogroll
-LINKS =  (('Pelican', 'http://getpelican.com/'),
-          ('Python.org', 'http://python.org/'),
-          ('Jinja2', 'http://jinja.pocoo.org/'),
-          ('You can modify those links in your config file', '#'),)
+LINKS =  (('My GitHub', 'https://github.com/slava'),
+          ('Me on Twitter', 'https://twitter.com/imslavko'),
+          ('More about me', 'http://slv.io/'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('twitter', 'https://twitter.com/imslavko'),
+          ('github', 'https://github.com/slava'),)
 
 DEFAULT_PAGINATION = 10
 
-# Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+# Theme
+THEME = './pelican-svbtle'
+DISPLAY_CATEGORIES_ON_MENU = False
+
